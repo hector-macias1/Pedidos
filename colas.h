4ea023aba31da6e56dc183579be2de60fce2c0af
hector-macias1 <hmacias54@uabc.edu.mx>
2022-10-30 00:20:31 -0700

    //header modificado

--- colas.h
+++ colas.h
@@ -18,12 +18,12 @@ struct NODE
 
 // Prototipos
 void enqueue(struct NODE **front, struct NODE **rear, struct NODE nodo);
-struct NODE dequeue(struct NODE **front, struct NODE **rear, struct NODE nodo);
-int queue_isempty();
-int queue_isfull();
-int queue_size();
-struct NODE front();
-struct NODE rear();
-void print_queue();
+struct NODE dequeue(struct NODE **front, struct NODE **rear);
+int queue_isempty(struct NODE **front, struct NODE **rear);
+int queue_isfull(struct NODE **front, struct NODE **rear, int limit);
+int queue_size(struct NODE **front, struct NODE **rear);
+struct NODE peek(struct NODE **front, struct NODE **rear);
+struct NODE rear(struct NODE **front, struct NODE **rear);
+void print_queue(struct NODE **front, struct NODE **rear);
 
 #endif
